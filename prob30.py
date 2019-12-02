8c26c24085b6d0e563cee49e4372fdf80970b5c1
Mark Davis <zymurge@gmail.com>
2019-12-01 19:35:14 -0800

    Rewrote for recursion

--- prob30.py
+++ prob30.py
@@ -1,16 +1,21 @@
-"""
-    For a given list of sequential integers, return True if any 3 members form an arithmetic sequence
-"""
-def has_sequence(numbers):
-    if len(numbers) < 3: return False 
+def has_sequence(numbers, end):
+    """ For a given list of sequential integers, return True if any 3 members form an arithmetic sequence.
+        Assess the list from the start through to the specified end, ignoring members beyond that limit.
+        Will short circuit and return False if there are less than 3 non-zero numbers to begin the sequence.
+    """
+    if end < 3 | len(numbers) < end:
+        return False 
 
-    i_sl = numbers[0:len(numbers)-2]
+    # Need the search sets to be inclusive of the end
+    end += 1
+
+    i_sl = numbers[0:end-2]
     #print("..i range:", i_sl)
     for i in enumerate(i_sl):
-        j_sl = numbers[i[0]+1:len(numbers)-1]
+        j_sl = numbers[i[0]+1:end-1]
         #print("..j range:", j_sl)
         for j in enumerate(j_sl, i[0]+1):
-            k_sl = numbers[j[0]+1:len(numbers)]
+            k_sl = numbers[j[0]+1:end]
             #print("..k range:", k_sl)
             for k in enumerate(k_sl, j[0]+1):
                 #print("....check:", i[1], j[1], k[1])
@@ -18,20 +23,55 @@ def has_sequence(numbers):
 
     return False
 
-"""
-    Find the n+1th integer from a passed in set of n
-    Will incrementally check from the specified start point and up to 20
-    returns the first match or 0 no valid options found
-"""
-def find_next(existing, start):
-    # Extend list to hold the candidate integer
-    mapped = existing.copy()
-    mapped.append(0)
-    for i in list(range(start,21)):
-        mapped[len(mapped)-1] = i
-        if not has_sequence(mapped):
+def find_next(existing, pos):
+    """ Finds the next valid integer that fits the sequence.
+        Starts at position 'pos' from the passed in set and find the next fitting integer under 21.
+        Assumes lowest value initial value is 0 and will not exceed 20. Updates 'existing' with the 
+        value found and returns that value, or 0 if no valid options found.
+    """
+    # For a beginning value, start with the previous positions value, since numbers must be sequentials
+    if existing[pos] < existing[pos - 1]:
+        existing[pos] = existing[pos - 1]
+
+    for i in range(existing[pos]+1,21):
+        existing[pos] = i
+        if not has_sequence(existing, pos):
             return i
+    existing[pos] = 0
     return 0
+
+def find_next_recursive(target):
+    """ Finds the first solution by iterating from the first position containing a 0.
+        Recursively called for each subsequent position out to the 9th.
+        Returns a valid array of integers or 0 if none found.
+    """
+    # determine position of first zero value
+    pos = 0
+    for i,v in enumerate(target):
+        if v == 0:
+            pos = i
+            break
+    
+    # loop through each valid value
+    find_next(target, pos)
+    # if 9th (zero based) position return solution
+    if target[8] != 0:
+        return target
+        
+    while target[pos] > 0:
+        # recurse until 9th position is filled
+        if find_next_recursive(target) == 0:
+            find_next(target, pos)
+        else:
+            return target
+
+    # fall through fail
+    return 0
+
+def solve_recursive():
+    target = [0] * 9
+    return find_next_recursive(target) 
+
 
 def solve_nested():
     for i1 in range(1, 13):
@@ -84,6 +124,6 @@ def solve_nested():
     return 0
 
 def run():
-    return solve_nested()         
+    return solve_recursive()         
 
 
--- test_functions.py
+++ test_functions.py
@@ -4,38 +4,69 @@ from mensa.prob30 import find_next
 from mensa.prob30 import has_sequence
 from mensa.prob30 import run
 
-
 class TestHasSequence(unittest.TestCase):
     def test_input_min(self):
         """
         Test that it fails on a list shorter than 3 ints
         """
-        dataset = [ [1, 2],[],[5] ]
+        dataset = [
+            ([1, 2], 2, False),
+            ([],1, False),
+            ([5], 1, False)
+        ]
         for case in dataset:
-            result = has_sequence(case)
-            self.assertEqual(result, False)
+            seq, end, expect = case
+            result = has_sequence(seq, end)
+            self.assertEqual(result, expect)
 
     def test_mixed_valid_cases(self):
         """
         Test that it fails on a list shorter than 3 ints
         """
-        dataset = [ ([1, 2, 4],False), ([5, 6, 7],True), ([1, 2, 4, 7, 8, 10, 22],True), ([1, 2, 4, 9, 10, 12],False) ]
+        dataset = [ 
+            ([1, 2, 4, 0, 0, 0], 2, False),
+            ([5, 6, 7, 0], 2, True),
+            ([1, 2, 4, 7, 8, 10, 22, 0, 0], 6, True),
+            ([1, 2, 4, 9, 10, 12], 5, False) 
+        ]
         for case in dataset:
-            result = has_sequence(case[0])
-            self.assertEqual(result, case[1])
+            seq, end, expect = case
+            result = has_sequence(seq, end)
+            self.assertEqual(result, expect)
 
 class TestFindNext(unittest.TestCase):
+    """ Finds the next valid integer that fits the sequence.
+        Starts at position 'pos' from the passed in set and find the next fitting integer under 21.
+        Assumes lowest value initial value is 0 and will not exceed 20. Updates 'existing' with the 
+        value found and returns that value, or 0 if no valid options found.
+    """
     def test_next_number_found(self):
-        dataset = [ ([1, 2, 4], 5, 5), ([1, 2, 4], 6, 8), ([1, 3, 9, 14, 16], 18, 20) ]
+        dataset = [ 
+            ([1, 3, 0, 0, 0, 0, 0], 1, [1, 4, 0, 0, 0, 0, 0], 4), 
+            ([1, 2, 4, 0, 0, 0, 0], 2, [1, 2, 5, 0, 0, 0, 0], 5), 
+            ([1, 3, 0, 0, 0, 0, 0], 2, [1, 3, 4, 0, 0, 0, 0], 4), 
+            ([1, 3, 8, 14, 16, 0, 0, 0, 0], 4, [1, 3, 8, 14, 17, 0, 0, 0, 0], 17),
+            ([1, 8, 9, 13, 16, 0, 0, 0, 0], 5, [1, 8, 9, 13, 16, 20, 0, 0, 0], 20)   
+        ]
+        
         for case in dataset:
-            result = find_next(case[0], case[1])
-            self.assertEqual(result, case[2])
+            exist, pos, update, expect = case # explicit names for clarity
+            result = find_next(exist, pos)
+            self.assertEqual(result, expect)
+            self.assertEqual(exist, update)
 
-    def test_next_number_none(self):
-        dataset = [ ([1, 2, 4, 7, 8, 10], 11, 0), ([12, 14, 15, 17], 18, 0) ]
+    def test_next_number_not_found(self):
+        dataset = [ 
+            ([1, 2, 20, 0, 0, 0, 0], 2, [1, 2, 0, 0, 0, 0, 0], 0), 
+            ([1, 3, 8, 14, 16, 19, 0], 6, [1, 3, 8, 14, 16, 19, 0], 0),
+            ([1, 3, 10, 15, 18, 0, 0, 0, 0], 5, [1, 3, 10, 15, 18, 0, 0, 0, 0], 0) 
+        ]
+        
         for case in dataset:
-            result = find_next(case[0], case[1])
-            self.assertEqual(result, case[2])
+            exist, pos, update, expect = case # explicit names for clarity
+            result = find_next(exist, pos)
+            self.assertEqual(result, expect)
+            self.assertEqual(exist, update)
 
 class TestRun(unittest.TestCase):
     def test_it(self):
